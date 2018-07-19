e030ed1c70bb43d11d8497bd06cf559611a0600b
liweiwei <2622026762@qq.com>
2018-07-19 12:36:29 +0800

    20180719

---
+++ README.md
@@ -0,0 +1,3 @@
+### mybatis 代码生成器 + gradle
+
+实现生成entity example mapper mapper.xml
---
+++ src/main/java/MybatisGeneratorApp.java
@@ -0,0 +1,13 @@
+import org.mybatis.generator.api.ShellRunner;
+
+/**
+ * 执行类
+ */
+public class MybatisGeneratorApp {
+
+    public static void main(String[] args) {
+
+        args = new String[]{ "-configfile", "src\\main\\resources\\generator.xml", "-overwrite" };
+        ShellRunner.main(args);
+    }
+}

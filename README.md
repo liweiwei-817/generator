01c7c996c2f85646fc989ffa09765cd7b24c292a
liweiwei <2622026762@qq.com>
2018-07-19 13:17:05 +0800

    20180719

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### mybatis 代码生成器 + gradle
 
 实现生成entity example mapper mapper.xml
+
+运行方式：
+gradle -> tasks -> application -> run
--- src/main/java/MybatisGeneratorApp.java
+++ src/main/java/MybatisGeneratorApp.java
@@ -7,7 +7,7 @@ public class MybatisGeneratorApp {
 
     public static void main(String[] args) {
 
-        args = new String[]{ "-configfile", "src\\main\\resources\\generator.xml", "-overwrite" };
+        args = new String[]{ "-configfile", "src/main/resources/generator.xml", "-overwrite" };
         ShellRunner.main(args);
     }
 }
